c7b36754a12a479add7a444052239a5825c9a339
Yuli <ytsypin@gmail.com>
2018-09-18 18:50:39 +0300

    Servlets mapped

--- NinaRowWebApp/web/NinaRow/pages/waitingroom/waitingroom.js
+++ NinaRowWebApp/web/NinaRow/pages/waitingroom/waitingroom.js
@@ -6,28 +6,6 @@ var LOAD_GAME_URL = "/NinaRow/loadGame";
 var CHAT_LIST_URL = "/NinaRow/chat";
 
 
-// TODO - This
-function refreshLoginStatus(){
-    $.ajax({
-        url:'login',
-        data:{
-            action:"status"
-        },
-        type:'GET',
-        success: statusCallback
-    });
-}
-
-function statusCallback(json){
-    if(!json.isConnected){
-        window.location = "index.html";
-    } else if(json.gameNumber != -1){
-        window.location = "waitingroom.html";
-    } else {
-        $('.userNameSpan').text("Welcome " + window.username + ", you are a " + (json.isComputer ? "computer" : "human"));
-    }
-}
-
 function loadGameClicked(event){
     var file = event.target.files[0];
     var reader = new FileReader();
